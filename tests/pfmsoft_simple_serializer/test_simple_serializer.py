16197463a16bd6aa5cd5861876d309c4d45a6b1a
Chad Lowe <pfmsoft@gmail.com>
2024-11-26 15:53:41 -0700

    gate yaml test, add doc comments

--- tests/pfmsoft_simple_serializer/test_simple_serializer.py
+++ tests/pfmsoft_simple_serializer/test_simple_serializer.py
@@ -1,25 +1,34 @@
+"""Tests for the simple serializer."""
+
+import logging
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import TypedDict
 
+import pytest
+
 from pfmsoft.simple_serializer.simple_serializer import DataclassSerializer
-import logging
 
 logger = logging.getLogger(__name__)
 
 
 @dataclass
 class DCTestClass:
+    """Dataclass test data holder."""
+
     name: str
     items: list[str] = field(default_factory=list)
 
 
 class DCTestClassTD(TypedDict):
+    """TypedDict test data holder."""
+
     name: str
     items: list[str]
 
 
 def test_to_simple():
+    """Test roundtrip to simple."""
     serializer = DataclassSerializer[DCTestClass, DCTestClassTD](
         complex_factory=lambda x: DCTestClass(**x)
     )
@@ -31,30 +40,30 @@ def test_to_simple():
     assert recomplicated_dc == dc
 
 
-def test_to_json(test_output_dir: Path):
-    # logger.info("trying to save json")
-    dc = DCTestClass(name="bob", items=["chair", "table"])
-    serializer = DataclassSerializer[DCTestClass, DCTestClassTD](
-        complex_factory=lambda x: DCTestClass(**x)
-    )
-    path_out = test_output_dir / "test_to_json" / "output.json"
-    serializer.save_as_json(path_out=path_out, complex_obj=dc, overwrite=True)
+# def test_to_json(test_output_dir: Path):
+#     # logger.info("trying to save json")
+#     dc = DCTestClass(name="bob", items=["chair", "table"])
+#     serializer = DataclassSerializer[DCTestClass, DCTestClassTD](
+#         complex_factory=lambda x: DCTestClass(**x)
+#     )
+#     path_out = test_output_dir / "test_to_json" / "output.json"
+#     serializer.save_as_json(path_out=path_out, complex_obj=dc, overwrite=True)
 
 
-def test_list_to_json(test_output_dir: Path):
-    # logger.info("trying to save json")
-    dc = DCTestClass(name="bob", items=["chair", "table"])
-    dc2 = DCTestClass(name="Richard", items=["Billard balls", "cue stick"])
-    items = [dc, dc2]
-    serializer = DataclassSerializer[DCTestClass, DCTestClassTD](
-        complex_factory=lambda x: DCTestClass(**x)
-    )
-    path_out = test_output_dir / "test_list_to_json" / "output.json"
-    serializer.save_iter_as_json(path_out=path_out, complex_obj=items, overwrite=True)
+# def test_list_to_json(test_output_dir: Path):
+#     # logger.info("trying to save json")
+#     dc = DCTestClass(name="bob", items=["chair", "table"])
+#     dc2 = DCTestClass(name="Richard", items=["Billard balls", "cue stick"])
+#     items = [dc, dc2]
+#     serializer = DataclassSerializer[DCTestClass, DCTestClassTD](
+#         complex_factory=lambda x: DCTestClass(**x)
+#     )
+#     path_out = test_output_dir / "test_list_to_json" / "output.json"
+#     serializer.save_iter_as_json(path_out=path_out, complex_obj=items, overwrite=True)
 
 
 def test_list_from_json(test_output_dir: Path):
-    # logger.info("trying to save json")
+    """Test roundtrip json."""
     dc = DCTestClass(name="bob", items=["chair", "table"])
     dc2 = DCTestClass(name="Richard", items=["Billard balls", "cue stick"])
     items = [dc, dc2]
@@ -68,7 +77,11 @@ def test_list_from_json(test_output_dir: Path):
 
 
 def test_list_from_yaml(test_output_dir: Path):
-    # logger.info("trying to save json")
+    """Test round trip to yaml."""
+    try:
+        import yaml  # type: ignore
+    except ImportError:
+        pytest.skip("yaml not installed.")
     dc = DCTestClass(name="bob", items=["chair", "table"])
     dc2 = DCTestClass(name="Richard", items=["Billard balls", "cue stick"])
     items = [dc, dc2]
@@ -76,6 +89,7 @@ def test_list_from_yaml(test_output_dir: Path):
         complex_factory=lambda x: DCTestClass(**x)
     )
     path_out = test_output_dir / "test_list_from_yaml" / "output.yaml"
+
     serializer.save_iter_as_yaml(path_out=path_out, complex_obj=items, overwrite=True)
     loaded_items = serializer.load_from_yaml_list(path_in=path_out)
     assert loaded_items == items
