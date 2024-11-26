0a9dde04295490ecb4fd742b6078cdfaac8fdfa3
Chad Lowe <pfmsoft@gmail.com>
2024-11-26 16:01:42 -0700

    release 0.1.0

--- src/pfmsoft/simple_serializer/__init__.py
+++ src/pfmsoft/simple_serializer/__init__.py
@@ -5,7 +5,7 @@ from pfmsoft.simple_serializer.simple_serializer import DataclassSerializer
 __author__ = "Chad Lowe"
 __email__ = "pfmsoft.dev@gmail.com"
 # The short X.Y.Z version.
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 # The full version, including alpha/beta/rc tags.
 __release__ = __version__
 
